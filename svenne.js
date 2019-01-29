cde304ce7b73f623eccfac48bd62cf05f959c941
Stefan Brus <stefan1brus@gmail.com>
2019-01-29 14:05:43 +0100

    Add command line args parsing and fix some styling

--- svenne.js
+++ svenne.js
@@ -1,9 +1,25 @@
+/**
+ * Svenne bot main entry point
+ *
+ * Author: Stefan Brus (github.com/stefan-brus)
+ * Copyright: Yes
+ * License: Beerware
+ */
+
+/**
+ * RTMClient is used to connect to slack's real time messaging API
+ */
+
 const { RTMClient } = require('@slack/client');
 
-// TODO: Read from file
-const token = 'bluff';
+/**
+ * `yargs` is used for simple command line argument parsing.
+ */
 
-const bot_user_id = 'UFTAL8WH4';
+const argv = require('yargs').argv;
+
+const token = argv.token;
+const botUserId = argv.botuser;
 
 // Connect to slack
 const rtm = new RTMClient(token);
@@ -11,12 +27,12 @@ rtm.start();
 
 rtm.on('message', (message) => {
     // Skip messages from myself
-    if(message.user === bot_user_id) {
+    if(message.user === botUserId) {
         return;
     }
 
     // Respond to messages mentioning me
-    if(message.text.includes(`<@${bot_user_id}>`)) {
+    if(message.text.includes(`<@${botUserId}>`)) {
         rtm.sendMessage('börk börk', message.channel)
             .then((res) => {
                 console.log('Message sent: ', res.ts);
